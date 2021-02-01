e999de6fa949fec33618dda01604b54cf11f4a1b
Nikita Budkovskiy <sonyfan23@gmail.com>
2021-02-02 02:29:36 +0300

    no message

--- build/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) {
-    res.send('The sedulous hyena ate the antelope!');
-});
-//# sourceMappingURL=app.js.map
